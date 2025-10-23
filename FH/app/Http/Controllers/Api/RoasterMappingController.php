b95596f2d7b3955b222a9b304f98c5fdd4d879b4
vidhya-eswaran <vidhya.eswaran@eucto.com>
2025-10-23 18:04:21 +0530

    route api change

--- FH/app/Http/Controllers/Api/RoasterMappingController.php
+++ FH/app/Http/Controllers/Api/RoasterMappingController.php
@@ -221,6 +221,8 @@ class RoasterMappingController extends Controller
         foreach ($chunkedHospitals as $chunkIndex => $chunk) {
             $destinations = $chunk->map(fn($h) => "{$h->latitude},{$h->longitude}")->implode('|');
 
+            dd($destinations);
+
             $response = Http::timeout(20)->get('https://maps.googleapis.com/maps/api/distancematrix/json', [
                 'origins' => "{$userLat},{$userLong}",
                 'destinations' => $destinations,
